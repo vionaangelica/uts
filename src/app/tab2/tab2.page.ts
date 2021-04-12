933bfa6e2f9ff0af53112458499c43218f2a1abd
Vio2x <c14180157@john.petra.ac.id>
2021-04-12 19:49:26 +0700

    commit on 'master'

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  private question;
+  private inputNum;
+  profpic = "https://www.mashbox.org/wp-content/uploads/2017/03/panda9.jpg";
 
-  constructor() {}
+  constructor() {
+    this.question = Math.floor(Math.random() * 10) + 1;
+    // alert(this.question)
+  }
 
+  ngOnInit(){
+    document.getElementById('Profile').style.display = 'none';
+  }
+
+  check(){
+    if (this.inputNum == this.question){
+      document.getElementById('Profile').style.display = 'block';
+      alert("Congratulations! You did it :D")
+    }
+    else{
+      alert("It's not too late to try again!")
+    }
+  }
 }
